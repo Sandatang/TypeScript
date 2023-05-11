02fcbb557387058c1997265d90f1842d26c953af
Sandatang <mushi.elite@gmail.com>
2023-05-11 20:33:05 +0800

    MERN To-Do Added React-BootStrap

--- to_do/frontend/src/App.tsx
+++ to_do/frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Note as NoteModel } from './models/notes';
 import Note from './components/Note';
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import style from "./styles/NotePage.module.css"
+import * as NoteApi from "./network/notes_api"
+import AddForm from './components/AddFormDialog';
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([])
 
+  const [showAddFormDialog, setShowAddFormDialog] = useState(false)
+
   useEffect(() => {
     async function loadNotes() {
       try {
-        const response = await fetch("/api/notes", { method: "GET" })
-        const notes = await response.json()
+        const notes = await NoteApi.fetchNotes()
         setNotes(notes)
       } catch (error) {
         console.error(error)
@@ -23,6 +26,9 @@ function App() {
 
   return (
     <Container>
+      <Button onClick={() => setShowAddFormDialog(true)}>
+        add note
+      </Button>
       <Row xs={1} md={2} xl={3} className='g-4'>
       {notes.map(note => {
         return (
@@ -32,6 +38,9 @@ function App() {
         )
       })}
       </Row>
+      {showAddFormDialog &&
+        <AddForm onDismiss={() => setShowAddFormDialog(false)}/>
+      }
     </Container>
   );
 }
